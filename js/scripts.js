ebd846d1979eab325631bd178b02c962b9095bac
Dave Sarbora and Shawn Lunsford <shawn@>
2019-01-28 09:52:45 -0800

    add business logic in js for AddressBook and contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-// ### Business / Back-End Logic
+// ### Business / Back-End Logic for AddressBook -----------
+function AddressBook() {
+  this.contacts = []
+  this.currentId= 0
+};
 
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+
+// ### Business / Back-End Logic for Contacts -----------
+function Contact(firstName,lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+};
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
 
 // ### User / Front-End Logic
 $(document).ready(function() {
