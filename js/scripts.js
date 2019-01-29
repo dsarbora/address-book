ec244db58996f1cf33299de3a2793bde6e2c6b95
Dave Sarbora and Jasmine Lee <jasmine@>
2019-01-29 13:34:29 -0800

    add javascript logic and html classes

--- js/scripts.js
+++ js/scripts.js
@@ -38,10 +38,13 @@ AddressBook.prototype.deleteContact = function(id){
 };
 
 // ### Business / Back-End Logic for Contacts -----------
-function Contact(firstName,lastName, phoneNumber) {
+function Contact(firstName,lastName, phoneNumber, email, address, address2) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.email = email;
+  this.address = address;
+  this.address2 = address2;
 };
 
 Contact.prototype.fullName = function() {
@@ -59,13 +62,26 @@ function displayContactDetails(addressBookToDisplay) {
   });
   contactsList.html(htmlForContactsInfo);
 }; 
+var fields = [".first-name", ".last-name", ".phone-number", ".email", ".address", ".address2"];
+function cleanFields(){
+  fields.forEach(function(field){
+    var fieldVal = $("span"+field).html();
+    if(fieldVal == ''){
+      $("p"+field).hide();
+    }
+  });
+};
 
 function showContact(contactID){
   var contact = addressBook.findContact(contactID);
-  $(".first-name").html(contact.firstName);
-  $(".last-name").html(contact.lastName);
-  $(".phone-number").html(contact.phoneNumber);
+  $("span.first-name").html(contact.firstName);
+  $("span.last-name").html(contact.lastName);
+  $("span.phone-number").html(contact.phoneNumber);
+  $('span.email').html(contact.email);
+  $('span.address').html(contact.address);
+  $('span.address2').html(contact.address2);
   $("#show-contact").show();
+  $("p").show();
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class = 'deleteButton' id =" + contact.id + ">Delete</button>");
@@ -80,6 +96,7 @@ function hideContact(contactID){
 function attachContactListeners(){
   $("ul#contacts").on("click", "li", function(){
     showContact(this.id);
+    cleanFields();
   }); //end ul#contacts.on("click")
   $("#buttons").on("click", ".deleteButton", function(){
     hideContact(this.id)
@@ -94,10 +111,16 @@ $(document).ready(function() {
     var inputtedFirstName = $("#new-first-name").val();
     var inputtedLastName = $("#new-last-name").val();
     var inputtedPhoneNumber = $("#new-phone-number").val();
+    var inputtedEmail = $("#new-email").val();
+    var inputtedAddress = $("#new-address").val();
+    var inputtedAddress2 = $("#new-address2").val();
     $("#new-first-name").val('');
     $("#new-last-name").val('');
     $("#new-phone-number").val('');
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    $("#new-email").val('');
+    $("#new-address").val('');
+    $("#new-address2").val('');
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedAddress, inputtedAddress2);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   });
